66f77ea8ed8115ba07207967b9e2dbf171934452
hazey <hazey@DESKTOP-EUHP70T.localdomain>
2026-04-12 01:30:31 +0100

    update once-ui.config.ts

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -14,7 +14,7 @@ import {
 import { home } from "./index";
 
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL: string = "https://haze-portfolio.vercel.app";
+const baseURL: string = "https://haze-crypt.vercel.app";
 
 const routes: RoutesConfig = {
   "/": true,
